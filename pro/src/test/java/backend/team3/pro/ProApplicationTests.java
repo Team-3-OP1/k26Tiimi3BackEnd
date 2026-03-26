42f1470055cb66e92488e518253fa3a510f271ed
AndreyKhoroshev <andrey.khoroshev@myy.haaga-helia.fi>
2026-03-26 15:55:07 +0200

    Epmty maven project created

---
+++ pro/src/test/java/backend/team3/pro/ProApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.team3.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
