9133aa64262b35101663349cd9b31a9f22a2a530
Razvan Hango <bhu212@myy.haaga-helia.fi>
2026-03-31 17:13:55 +0300

    edit button  toimii

--- src/main/java/backend/team3/pro/Controller/IndexController.java
+++ src/main/java/backend/team3/pro/Controller/IndexController.java
@@ -1,9 +1,9 @@
 package backend.team3.pro.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,30 +12,44 @@ import backend.team3.pro.Repository.VaateRepository;
 
 @Controller
 public class IndexController {
+    public final VaateRepository repository;
 
-    @Autowired
-    private VaateRepository repository;
+    public IndexController(VaateRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/index")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vaate", new Vaate());
+        return "index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editVaate(@PathVariable("id") Long id, Model model) {
+        Vaate vaate = repository.findById(id).orElse(null);
+        model.addAttribute("vaate", vaate);
         return "index";
     }
 
     @PostMapping("/index")
-    public String saveVaatte(@RequestParam("name") String name,
+    public String saveVaatte(
+            @RequestParam(value = "id", required = false) Long id,
+            @RequestParam("name") String name,
             @RequestParam("size") String size,
             @RequestParam("price") double price) {
 
-        Vaate newVaatte = new Vaate(name, size, price);
-        repository.save(newVaatte);
+        Vaate vaate;
+        if (id != null) {
+            vaate = repository.findById(id).orElse(new Vaate());
+            vaate.setName(name);
+            vaate.setSize(size);
+            vaate.setPrice(price);
+        } else {
+            vaate = new Vaate(name, size, price);
+        }
+        repository.save(vaate);
 
         return "redirect:/homepage";
-    }
-
-    @GetMapping("/homepage")
-    public String showHomepage(Model model) {
-        model.addAttribute("vaatteet", repository.findAll());
-        return "Homepage";
     }
 
 }
