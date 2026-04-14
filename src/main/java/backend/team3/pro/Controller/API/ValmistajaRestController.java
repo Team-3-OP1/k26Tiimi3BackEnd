3efe6dafa7825e3e92d1eb0994d762b4688637a7
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2026-04-14 22:18:47 +0300

    Lisätty REST-rajapinta valmistajille

--- src/main/java/backend/team3/pro/Controller/API/ValmistajaRestController.java
+++ src/main/java/backend/team3/pro/Controller/API/ValmistajaRestController.java
@@ -1,8 +1,25 @@
 package backend.team3.pro.Controller.API;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import backend.team3.pro.Model.Valmistaja;
+import backend.team3.pro.Repository.ValmistajaRepository;
 
 @RestController
 public class ValmistajaRestController {
 
+    private final ValmistajaRepository valmistajaRepository;
+
+    public ValmistajaRestController(ValmistajaRepository valmistajaRepository) {
+        this.valmistajaRepository = valmistajaRepository;
+    }
+
+    @GetMapping("/api/valmistajat")
+    public @ResponseBody List<Valmistaja> valmistajaListRest() {
+        return (List<Valmistaja>) valmistajaRepository.findAll();
+    }
 }
