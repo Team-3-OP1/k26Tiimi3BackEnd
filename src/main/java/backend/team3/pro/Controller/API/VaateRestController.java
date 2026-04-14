97c8f8cc596aff71c52a5bf11620025e7ba5ee58
JoneHyttinen <jone.hyttinen@myy.haaga-helia.fi>
2026-04-14 16:16:08 +0300

    REST -rajapinta vaatteille tehty

---
+++ src/main/java/backend/team3/pro/Controller/API/VaateRestController.java
@@ -0,0 +1,31 @@
+package backend.team3.pro.Controller.API;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import backend.team3.pro.Model.Vaate;
+import backend.team3.pro.Repository.VaateRepository;
+
+@RestController
+public class VaateRestController {
+
+    private final VaateRepository vaateRepository;
+
+    public VaateRestController(VaateRepository vaateRepository) {
+        this.vaateRepository = vaateRepository;
+    }
+
+    @GetMapping("/api/vaatteet")
+    public @ResponseBody List<Vaate> vaateListRest() {
+        return (List<Vaate>) vaateRepository.findAll();
+    }
+
+    @GetMapping("/api/vaate/{id}")
+    public @ResponseBody Vaate findVaateRest(@PathVariable("id") Long id) {
+        return vaateRepository.findById(id).orElse(null);
+    }
+}
---
+++ src/main/java/backend/team3/pro/Controller/API/ValmistajaRestController.java
@@ -0,0 +1,8 @@
+package backend.team3.pro.Controller.API;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ValmistajaRestController {
+
+}
